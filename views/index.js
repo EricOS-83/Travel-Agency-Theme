028ead43fc85050530adb9915fade6ab3e470d12
ErikOS <errikos.gioussef@gmail.com>
2024-10-20 16:53:57 +0300

    render index.ejs

--- views/index.js
+++ views/index.js
@@ -12,6 +12,10 @@ app.use(express.static("public"));
 //use bodyparser as middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// serve index.ejs when user reaching out, homepage
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+  })
 // Start the server
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
