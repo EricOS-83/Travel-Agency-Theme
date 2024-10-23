5792c965764ff4c9e43b53fcc96d99fb501a8ac6
ErikOS <errikos.gioussef@gmail.com>
2024-10-23 10:47:43 +0300

    create an image carouse in homepage

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+// Serve static files, like CSS, images, etc.
+app.use(express.static("public"));
+
+// Use body-parser as middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve the index.ejs when the user accesses the homepage
+app.get('/', (req, res) => {
+    res.render('index.ejs');  // Render index.ejs (you need to create this file in the views folder)
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
